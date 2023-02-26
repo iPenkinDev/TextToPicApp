c80278f9501931f13655602822341b2c3e3c5baf
Igor Penkin <igor.penkin.dev@gmail.com>
2023-02-26 19:36:30 +0300

    add Log4j

---
+++ src/main/java/com/dev/texttopicapp/TextToPicAppApplication.java
@@ -0,0 +1,13 @@
+package com.dev.texttopicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextToPicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextToPicAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/dev/texttopicapp/controller/TelegramBotController.java
@@ -0,0 +1,18 @@
+package com.dev.texttopicapp.controller;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+
+public class TelegramBotController extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+}
