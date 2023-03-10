aafc8bd25df1352532846c1b98a8b0afe6024182
Igor Penkin <igor.penkin.dev@gmail.com>
2023-03-10 21:52:20 +0300

    add controller + service

--- src/main/java/com/dev/texttopicapp/TextToPicAppApplication.java
+++ src/main/java/com/dev/texttopicapp/TextToPicAppApplication.java
@@ -2,14 +2,11 @@ package com.dev.texttopicapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class TextToPicAppApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(TextToPicAppApplication.class, args);
 	}
 
--- src/main/java/com/dev/texttopicapp/controller/TelegramBotController.java
+++ src/main/java/com/dev/texttopicapp/controller/TelegramBotController.java
@@ -1,24 +1,18 @@
 package com.dev.texttopicapp.controller;
 
-import com.dev.texttopicapp.config.TelegramBotConfig;
 import com.dev.texttopicapp.service.TelegramBotService;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import reactor.core.publisher.Mono;
-import reactor.netty.http.client.HttpClient;
 
 import java.text.MessageFormat;
 
-@RestController
+@Controller
 @Log4j
 public class TelegramBotController {
-
     private final TelegramBotService telegramBotService;
 
     @Autowired
@@ -26,9 +20,17 @@ public class TelegramBotController {
         this.telegramBotService = telegramBotService;
     }
 
-    @GetMapping("/sendMessage")
+    @PostMapping("/sendMessage")
     public Mono<String> sendMessage(@RequestParam String message) {
         log.debug(MessageFormat.format("Sending message: ", message));
         return telegramBotService.sendMessage(message);
     }
+    @PostMapping("/getUpdates")
+    public String getUpdates() {
+        log.debug(telegramBotService.getUpdates());
+        return telegramBotService.getUpdates();
+    }
+
+
+
 }
